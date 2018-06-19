8f017fd3a1c92d3f7e9190847885c8dfb7293904
Yuri Doronin <dye491@gmail.com>
2018-06-19 10:39:44 +0200

    create Airport model

---
+++ models/Airport.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "airport".
+ *
+ * @property int $id
+ * @property string $code
+ * @property string $name
+ * @property int $country
+ */
+class Airport extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'airport';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['country'], 'integer'],
+            [['code'], 'string', 'max' => 3],
+            [['name'], 'string', 'max' => 64],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'code' => 'Code',
+            'name' => 'Name',
+            'country' => 'Country',
+        ];
+    }
+}
